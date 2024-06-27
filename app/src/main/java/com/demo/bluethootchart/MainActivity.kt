2890e6d8a857de5bf2f7163d2b7d8db6a8ea86ff
Deepak <deepak.bachulkar@owision.com>
2024-06-27 13:22:47 +0530

    remove unwanted code

--- app/src/main/java/com/demo/bluethootchart/MainActivity.kt
+++ app/src/main/java/com/demo/bluethootchart/MainActivity.kt
@@ -82,21 +82,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    BluethootChartTheme {
-//        Greeting("Android")
-//    }
-//}
+
 
 
 
